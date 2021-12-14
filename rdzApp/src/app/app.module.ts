8a5cb5a0d2d1b186f8aeff7a1d8182f85f0e508f
Paul <paul.colditz@web.de>
2021-12-14 19:12:11 +0100

    again

--- rdzApp/src/app/app.module.ts
+++ rdzApp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainRoomComponent } from './components/main-room/main-room.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainRoomComponent
   ],
   imports: [
     BrowserModule,
