891528737b8d976024e3311b077f6c1bf7945c98
Paul <paul.colditz@web.de>
2022-01-04 20:32:06 +0100

    create room item and basic styling

--- rdzApp/src/app/app.module.ts
+++ rdzApp/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainRoomComponent } from './components/main-room/main-room.component';
+import { RoomItemComponent } from './components/room-item/room-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainRoomComponent
+    MainRoomComponent,
+    RoomItemComponent
   ],
   imports: [
     BrowserModule,
