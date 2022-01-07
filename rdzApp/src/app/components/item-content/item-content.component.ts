b174c8af978ff0461571a42660a478ebc2f4012e
Paul <paul.colditz@web.de>
2022-01-07 01:04:21 +0100

    dynamic style and visibility

--- rdzApp/src/app/components/item-content/item-content.component.ts
+++ rdzApp/src/app/components/item-content/item-content.component.ts
@@ -6,13 +6,15 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./item-content.component.scss'],
 })
 export class ItemContentComponent implements OnInit {
+  @Input() selected = false;
+  @Input() visible = true;
   @Input() title = 'Title';
   @Input() imageSrc = '';
-  @Input() imageStyle = { top: 38, left: 5, height: 50, width: 20 };
-  // 'top: 38%; left: 5%; max-height: 50%; max-width: 20%;';
+  @Input() imageStyle = 'top: 38%; left: 5%; max-height: 50%; max-width: 20%;';
 
   getItemImageStyle(): string {
-    return `top: ${this.imageStyle.top}%; left: ${this.imageStyle.left}%; max-height: ${this.imageStyle.height}%; max-width: ${this.imageStyle.width}%;`;
+    // return `top: ${this.imageStyle.top}%; left: ${this.imageStyle.left}%; max-height: ${this.imageStyle.height}%; max-width: ${this.imageStyle.width}%;`;
+    return this.imageStyle;
   }
 
   constructor() {}
--- rdzApp/src/app/components/room-item/room-item.component.ts
+++ rdzApp/src/app/components/room-item/room-item.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class RoomItemComponent implements OnInit {
   @Input() imagePath: string = '';
+  @Input() background = false;
 
   constructor() {}
 
