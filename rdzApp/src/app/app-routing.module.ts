050c44c87eb20e72ade10c9be1b6e44f7d99abed
Paul <paul.colditz@web.de>
2022-01-07 02:29:17 +0100

    add routing

--- rdzApp/src/app/app-routing.module.ts
+++ rdzApp/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainRoomComponent } from './components/main-room/main-room.component';
 
-const routes: Routes = [];
+// enum AllRoutes {
+//   idea,
+//   events
+// }
+
+const routes: Routes = [
+  { path: 'events', component: MainRoomComponent },
+  { path: 'idea', component: MainRoomComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- rdzApp/src/app/components/item-content/item-content.component.ts
+++ rdzApp/src/app/components/item-content/item-content.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-item-content',
@@ -6,18 +7,36 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./item-content.component.scss'],
 })
 export class ItemContentComponent implements OnInit {
-  @Input() selected = false;
-  @Input() visible = true;
   @Input() title = 'Title';
   @Input() imageSrc = '';
   @Input() imageStyle = 'top: 38%; left: 5%; max-height: 50%; max-width: 20%;';
+  @Input() route = 'idea';
+
+  private selected = false;
+  constructor(private router: Router) {}
 
   getItemImageStyle(): string {
     // return `top: ${this.imageStyle.top}%; left: ${this.imageStyle.left}%; max-height: ${this.imageStyle.height}%; max-width: ${this.imageStyle.width}%;`;
     return this.imageStyle;
   }
 
-  constructor() {}
+  itemClick() {
+    if (this.router.url === '/') {
+      this.router.navigateByUrl('/' + this.route);
+    } else {
+      this.router.navigateByUrl('/');
+    }
+  }
+
+  routeSelected(): boolean {
+    if (this.router.url === '/' + this.route) return true;
+    return false;
+  }
+
+  routeHome(): boolean {
+    if (this.router.url === '/') return true;
+    return false;
+  }
 
   ngOnInit(): void {}
 }
